9242c68b210e2de4c3809e32744ecab285cb282d
Admin <tctdat2007@gmail.com>
2025-10-19 19:51:13 +0700

    Write a program that asks the user how many dice to roll

---
+++ 1.py
@@ -0,0 +1,8 @@
+import random
+num_dice = int(input("How many dice would you like to roll? "))
+total = 0
+for i in range(num_dice):
+    roll = random.randint(1, 6)
+    total += roll
+
+print(f"The sum of the dice rolls is {total}.")
